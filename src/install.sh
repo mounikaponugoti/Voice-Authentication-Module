3ca81fb19f56718765855ae85f8c69b277ebe103
Mounika Ponugoti <mounikaponaganti@gmail.com>
2019-05-19 14:59:45 -0500

    Keep source files seperate

--- src/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run the script in /lib/voice_authen folder
-g++ voice_authentication.cpp -shared -o /lib/x86_64-linux-gnu/security/pam_voice_authentication.so -fPIC
-g++ train_users_voice.cpp -o /usr/bin/train_users_voice
